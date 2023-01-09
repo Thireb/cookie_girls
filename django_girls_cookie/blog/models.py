b975e62d61d7a4a8bbb84ccd54db688632ffca20
Thireb <muhammadahmadashiq@gmail.com>
2023-01-09 13:11:49 +0500

    modal updated, css added, post modal null=true done.

--- django_girls_cookie/blog/models.py
+++ django_girls_cookie/blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     
     """Model with name getting from loged in user, title of the post displayed as identifier, text of post, created at data which is taken at creation time automatically and published at date, which is taken when publish method is called. """
     
-    name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     title = models.CharField('Title of post',max_length=125)
     text = models.TextField('Enter the text here')
     created_at = models.DateTimeField(default=timezone.now)
--- django_girls_cookie/static/js/main.js
+++ django_girls_cookie/static/js/main.js
@@ -1,14 +1,13 @@
-var post_to_btn = ""; //because delete button is replicated and only that has the id.
-// so to get every button's id on click, a global variable has to be there.
+var post_to_btn = ""; /*because delete button is replicated and only that has the id. 
+So to get every button's id on click, a global variable has to be there.*/
+
 $("#myBtn").on("click", function (event) {
   post_to_btn = $(this).attr("post_id");
-  //console.log(post_to_btn);
   $("#myModal").modal("toggle");
 });
 
 $("#yes_delete_button").on("click", function (event) {
   postDelete();
-  //$("#myModal").modal("toggle");
 });
 
 function postDelete() {
@@ -17,65 +16,54 @@ function postDelete() {
     type: "POST",
     data: $(".post-form").serialize(),
     success: function (json) {
-      //alert('Post Deleted!')
-      //post_to_delete = ''
-      //console.log(json);
       console.log("success");
       var meriDiv = document.getElementById(post_to_btn);
-      //console.log(post_to_delete.type);
       meriDiv.parentNode.removeChild(meriDiv);
     },
     error: function (xhr, errmsg, err) {
       console.log(xhr);
       console.log(errmsg);
       console.log(err);
-      // $("#results").html(
-      //   "<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " +
-      //     errmsg +
-      //     " <a href='#' class='close'>&times;</a></div>"
-      // ); // add the error to the dom
-      // console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+    
     },
   });
 }
+function open_modal(){
+  $("#myModal").modal("show");
 
+}
+function hide_modal(){
+  $("#myModal").modal("hide");
+
+}
 $("#edit").on("click", function (event) {
-  //event.preventDefault();
-  //postUpdate();
-  $("#myModal").modal("toggle");
+  open_modal();
 });
 $("#yes_button").on("click", function (event) {
-  //event.preventDefault();
   var text_to_update = $("#id_text");
-  //console.log(text_to_update);
   var title_to_update = $("#id_title");
-  //console.log(title_to_update);
-  // var error_message = $("#error_display").val();
-
   //Check fields empty or not, if empty, disable the submit button.
+ 
   if (text_to_update.val().length > 0 && title_to_update.val().length > 0) {
     console.log('All Ok')
+    postUpdate(text_to_update.val(), title_to_update.val());
+    $(".error-msg").text("");
+
+    hide_modal();    
   } else {
-    console.log("Empty field");
-    // $(".invalid-feedback").html("<strong>Fields cannot be empty.</strong>");
-    // $("#error_display").text("Fields cannot be empty");
+    //Prevent the modal from closing
+      console.log("Empty field");
+    
+      // $("#myModal").on("hide.bs.modal", function (e) {
+      //   e.preventDefault();
+      // });
+      $(".error-msg").text("Fields cannot be empty");
 
-    $(".title_label").before("<p >Fields cannot be empty</p>");
-
-    $(this).prop("disabled", true);
-    //postUpdate();
+    
   }
-  //$("#myModal").modal("toggle");
 });
-function postUpdate() {
-  //console.log(post_to_update);
+function postUpdate(text_to_update, title_to_update) {
   console.log($("#yes_button").data("idpost"));
-  //post_to_update = $(this).attr("post_id");
-
-  var text_to_update = $("#id_text").val();
-  console.log(text_to_update);
-  var title_to_update = $("#id_title").val();
-  console.log(title_to_update);
   var post = $("#yes_button").data("idpost");
   $.ajax({
     headers: { "X-CSRFToken": $("[name=csrfmiddlewaretoken]").val() },
@@ -83,26 +71,18 @@ function postUpdate() {
     type: "POST",
     data: $(".post-form").serialize(),
     success: function (json) {
-      //console.log(json);
       console.log("success");
       $(".title_here").text(title_to_update);
       $(".text_here").text(text_to_update);
+
     },
     error: function (xhr, errmsg, err) {
+
+      //Display the error, critical ones, in console
       console.log(xhr);
       console.log(errmsg);
       console.log(err);
 
-      // $("#results").html(
-      //   "<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " +
-      //     errmsg +
-      //     " <a href='#' class='close'>&times;</a></div>"
-      // );
-      //console.log(xhr.status + ": " + xhr.responseText);
     },
   });
 }
-
-// function postUpdateCall(){
-//     postUpdate()
-// }
